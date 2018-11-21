c3881bf33e1c69d4531bbc62660d12773ad8b06d
zhouwei <zhouwei@huli.com>
2018-11-21 17:26:38 +0800

    项目初始化

---
+++ index.js
@@ -0,0 +1,28 @@
+const Koa = require('koa');
+const axios = require('axios');
+const app = new Koa();
+// logger
+app.use(async (ctx, next) => {
+   await next();
+});
+// x-res
+app.use (async (ctx, next) =>{
+   await next();
+});
+// response
+app.use(async (ctx, next) => {
+  await next();
+ // ctx.body = 'hello world'
+});
+app.use(async (ctx, next) => {
+  /*if (ctx.url === '/getUser') {
+    await axios.get('http://webapi.soydai.cn:3499/apitest/assets/common/getUser').then((response) => {
+      console.log('response',response.data);
+      ctx.response.body = response.data;
+    })
+
+    // next();
+  }*/
+  ctx.response.body = 'zhoooo'
+})
+app.listen(4000);
